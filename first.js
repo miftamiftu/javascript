6205ad04c16b15f33742098553f12906031ab4ab
Your Name <miftamiftuu23@gmail.com>
2026-06-25 23:57:57 +0600

    variable and dt

--- first.js
+++ first.js
@@ -1,3 +1,13 @@
+const student={
+    fullName : "Miftahul jannat",
+    age:20,
+    cgpa:3.2,
+};
+student["age"]=student["age"]+5
+
+console.log(student["age"]);
+console.log(student.fullName);
+
 console.log("My name is mifta");
 console.log("i want to learn english");
 
@@ -15,3 +25,23 @@ let y=undefined;
 console.log(y);
 let isFollow=true;
 console.log(isFollow);
+
+
+const product={
+    title:"Ball pen",
+    rating:4,
+    offer:5,
+    price:270,
+
+};
+console.log(product);
+
+const profile={
+    userName:"Miftuuu",
+    follower:1000,
+    following:234,
+    isFollow:false,
+
+};
+console.log(profile);
+console.log(typeof profile ["isFollow"]);
