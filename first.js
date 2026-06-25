291e752d760b8b591923e4f8f1eb1c8d8313d4ee
Your Name <miftamiftuu23@gmail.com>
2026-06-25 23:00:27 +0600

    variables

--- first.js
+++ first.js
@@ -1,2 +1,15 @@
 console.log("My name is mifta");
 console.log("i want to learn english");
+
+name="Mifta";
+console.log(name);
+
+
+age=24;
+price=34.56;
+console.log(age);
+console.log(price);
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
