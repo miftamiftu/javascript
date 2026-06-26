92a19f338db526c03d039765a103f0c141b42f14
Your Name <miftamiftuu23@gmail.com>
2026-06-26 11:19:05 +0600

    func2

--- function.js
+++ function.js
@@ -16,3 +16,28 @@ function sum(x,y){
 
 }
 sum(567,876);
+
+//arrow function
+function sum2(a,b){
+    return a+b;
+}
+const arrowSum2 = (a,b) => {
+    console.log(a+b);
+};
+
+const arrowMul=(c,d)=>{
+    console.log(c*d);
+};
+
+function countVowels(str){
+    let count=0;
+    for(const char of str ){
+        console.log(char);
+        if(char==="a"||char==="e"||char==="i"||char==="O"||char==="u"){
+            count++;
+        }
+    }
+    console.log(count)
+};
+
+
