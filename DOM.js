db5a24eb8a340a0fbeef2a4a10c8d821f42d1fe3
Your Name <miftamiftuu23@gmail.com>
2026-06-27 22:30:27 +0600

    f

--- DOM.js
+++ DOM.js
@@ -1,6 +1,12 @@
-let headings=document.getElementsByClassName("Head");
-console.log(headings);
-console.dir(headings);
+/*let h2=document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText=h2.innerText+"from apna college student";*/
 
-let elements=document.querySelector("p");
-console.dir(elements);
+let divs=document.querySelectorAll(".box");
+console.log(divs[0]);
+divs[0].innerText="new Mifta"
+console.log(divs[1]);
+divs[1].innerText="new life"
+console.log(divs[2]);
+divs[2].innerText="love yourself"
+
