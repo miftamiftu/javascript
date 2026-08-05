0891e2d6c72ce1e53ce6e385b2f15005431a37a4
Your Name <miftamiftuu23@gmail.com>
2026-08-05 19:23:27 +0600

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+let hard=document.querySelector("#hard");
+hard.onclick=() => {
+  console.log("hard was clicked");
+  let a=25;
+  a++;
+  console.log(a);
+}
+let hard=document.querySelector("#hard");
+hard.onclick=() => {
+  console.log("hard was clicked2");
+  let a=25;
+  a++;
+  console.log(a);
+}
