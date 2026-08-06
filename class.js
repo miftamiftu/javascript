a090f5437559ba66756141e4b5b0283b627ab16e
Your Name <miftamiftuu23@gmail.com>
2026-08-06 19:37:05 +0600

    basics

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
-const student = {
+/*const student = {
     fullName: "Miftahul jannat",
     marks:78,
     printMarks:function(){
@@ -42,3 +42,50 @@ setTimeout(()=>{
 
 console.log ("four");
 console.log ("five");
+
+
+//callback
+function sum(a,b){
+    console.log(a+b);
+}
+function calculator(a,b,sumCallback){
+    sumCallback(a,b);
+}
+calculator(3,5,sum);
+
+//promises
+
+let promise=new Promise((resolve,reject)=>{
+    console.log("im a promise");
+    resolve(123);
+    reject("some error");
+});
+
+function getData(dataId,getNextdata){
+    return new promise
+}
+
+
+const getPromise=()=>{
+    return new Promise((resolve,reject)=> {
+        console.log("im a promise");
+    //resolve("success");
+    reject("new error")
+});
+};
+let promise=getPromise();
+promise.then((res)=>{
+    console.log("fulfilled");
+
+});
+promise.catch((err)=>{
+
+console.log("rejected",err);
+});
+
+ */
+async function getWeather(){
+    await getdata (1);
+    await getdata (2);
+    await getdata (3);
+}
