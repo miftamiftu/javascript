d958e0aa41bf61fbec9598439df9a63cbbba698f
Your Name <miftamiftuu23@gmail.com>
2026-08-06 18:19:54 +0600

    sync

--- class.js
+++ class.js
@@ -12,8 +12,33 @@ class mifta{
         console.log("good");
     }
     stop(){
-        console.log("stop");
+        console.log("stop");}
+
+        setBrand(brand){
+    this.brandName=brand;
     }
 }
 
 let fortunate=new mifta();
+fortunate.setBrand=("fortunate");
+
+//synchronus
+console.log ("one");
+console.log ("two");
+console.log ("three");
+
+
+
+
+//asynchronus
+function hello(){
+    console.log("hello");
+}
+setTimeout(hello,2000);
+
+setTimeout(()=>{
+    console.log("mifta");
+},4000);//4s=timeout means kotokkhn wait korbr show korar jonne
+
+console.log ("four");
+console.log ("five");
